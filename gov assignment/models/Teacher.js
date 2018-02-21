f71e6750c30ac920a8e421256823944d674fdc62
TingWai <tingwai.wong@ufinity.com>
2018-02-21 16:38:10 +0800

    - add in mocha and chai testing

--- gov assignment/models/Teacher.js
+++ gov assignment/models/Teacher.js
@@ -1,22 +1,27 @@
 var db = require('../dbconnection'); //reference of dbconnection.js  
 var Teacher = {
-    getAllTasks: function (callback) {
-        var lala = db.query("Select * from school_teacher", callback);
-        console.log(lala);
-        return lala;
+    getAllStudents: function (callback) {
+        return  db.query("Select * from school_student", callback);
     },
     getTaskById: function (teacherEmail, callback) {
         return db.query("select * from school_teacher where teacherEmail=?", [teacherEmail], callback);
     },
     getStudents: function (teacher, callback) {
         console.log('the value =' + teacher);
-        return db.query("select * from school_teacher_student where teacherEmail in (?)", [teacher], callback);
+        return db.query("select studentEmail from school_teacher_student where teacherEmail in (?)", [teacher], callback);
     },
     addSuspendStudent: function (student, callback) {
         return db.query("INSERT INTO school_suspend_student (studentEmail) VALUES (?) ", [student.student], callback);
     },
     getStudentRecipients: function (teacher, studentList, callback) {
-        return db.query("select ts.studentEmail from school_teacher_student ts, school_suspend_student st where ts.studentEmail != st.studentEmail and ts.teacherEmail = ? and ts.studentEmail = ? ", [teacher, studentList], callback);
+        console.log('inside model');
+        console.log(teacher);
+        console.log(studentList);
+        console.log('hasdjhasjkdhasd here');
+        // var result = db.query("select ts.studentEmail from school_teacher_student ts, school_suspend_student st where ts.studentEmail != st.studentEmail and ts.teacherEmail = ? and ts.studentEmail in (?) ", [teacher, studentList], callback);
+        // console.log(result);
+
+        return db.query("select ts.studentEmail from school_teacher_student ts, school_suspend_student st where ts.studentEmail != st.studentEmail and ts.teacherEmail = ? or ts.studentEmail in (?) ", [teacher, studentList], callback);
     },
     registerStudent: function (Teacher, callback) {
         //console.log(Teacher);
--- gov assignment/routes/TeacherRouter.js
+++ gov assignment/routes/TeacherRouter.js
@@ -26,24 +26,39 @@ var Teacher = require('../models/Teacher');
 // });
 
 
-router.get('/', function (req, res, next) {
-    console.log('over here');
+router.get('/commonstudents', function (req, res, next) {
     console.log(req.query);
     console.log(req.query.teacher);
-    if (res.query) {
+    // query string was found
+    if (req.query.teacher != undefined) {
+        console.log('get specify student based on teacher');
         Teacher.getStudents(req.query.teacher, function (err, rows) {
             if (err) {
                 res.json(err);
             } else {
-                res.json(rows);
+                if (rows.length > 0) {
+                    var students = {};
+                    var bodyContent = [];
+                    for (var x = 0; x < rows.length; x++) {
+                        bodyContent.push(rows[x].studentEmail);
+                    }
+                    students['students'] = bodyContent;
+                    res.status(200).json(students);
+                }
+                else{
+                    res.status(204);
+                    res.json(rows);
+                }
+
             }
         });
     }
     else {
-        Teacher.getAllTasks(function (err, rows) {
+        Teacher.getAllStudents(function (err, rows) {
             if (err) {
                 res.json(err);
             } else {
+                console.log('get all students');
                 console.log(rows);
                 res.json(rows);
             }
@@ -52,7 +67,7 @@ router.get('/', function (req, res, next) {
 
 });
 
-router.post('/', function (req, res, next) {
+router.post('/register', function (req, res, next) {
     Teacher.registerStudent(req.body, function (err, count) {
         if (err) {
             console.log(err);
@@ -78,17 +93,54 @@ router.post('/suspend', function (req, res, next) {
 });
 router.post('/retrievefornotifications', function (req, res, next) {
     console.log(req.body);
-    // Teacher.getStudentRecipients(req.body, function (err, count) {
-    //     if (err) {
-    //         console.log(err);
-    //         res.status(500);
-    //         res.json(err);
-    //     } else {
-    //         res.json(req.body); //or return count for 1 & 0  
-    //     }
-    // });
-});
+    var startWithMention = false;
+    var endWithMentionCheck = false;
+    var endWithMention = '';
+    var studentEmail = '';
+    var studentEmailList = [];
 
+    for (var x = 0; x < req.body.notification.length; x++) {
+        if (startWithMention) {
+            studentEmail += req.body.notification.charAt(x);
+            // first regex is @gmail and should end with .com
+            if (studentEmail.match('[@]{2,}')) {
+                console.log('invalid email address');
+                return;
+            }
+            else if (studentEmail.length >= 50) {
+                console.log('error occur');
+                return;
+            }
+            else if (studentEmail.match('[@]{1,}[a-zA-Z]') && studentEmail.endsWith('.com')) {
+                studentEmailList.push(studentEmail);
+                startWithMention = false;
+                studentEmail = '';
+                console.log(studentEmailList);
+            }
+        }
+        if (!startWithMention && req.body.notification.charAt(x) === "@") {
+            startWithMention = true;
+        }
+    }
+
+    Teacher.getStudentRecipients(req.body.teacher, studentEmailList, function (err, count) {
+        if (err) {
+            console.log(err);
+            res.status(500);
+            res.json(err);
+        } else {
+            var recipients = {};
+            var bodyContent = [];
+            for (var x = 0; x < count.length; x++) {
+                bodyContent.push(count[x].studentEmail);
+            }
+            console.log(bodyContent);
+            recipients['recipients'] = bodyContent;
+            console.log(recipients);
+            res.json(recipients);
+        }
+    });
+});
 
 
 module.exports = router;  
